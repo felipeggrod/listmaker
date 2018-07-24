308d9c1386dc3085a66721f7465e2d9ae0ff7dc2
Felipe Rodrigues <felipergr@hotmail.com>
2018-07-24 01:15:58 -0300

    Recursively accessing all json fields from itemList

---
+++ src/components/ItemElement.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ItemElement = (props) => {
+    return (
+        <div> life is beautiful </div>
+    );
+}
+export default ItemElement;
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
+
+var itemListArray = [];
+
+function RunList(obj, path)
+{
+    var txt = '';
+    for (var key in obj)
+    {
+        if (obj.hasOwnProperty(key))
+        {
+            if ('object' === typeof(obj[key]))
+            {
+                //txt += RunList(obj[key], path + (path ? '.' : '') + key);
+                itemListArray.push(<div>AAA</div>);
+                console.log('if: '+obj[key]);
+                RunList(obj[key], '');
+            } 
+            else
+            {
+                console.log('else: '+obj[key]);
+                //txt += path + '.' + key + '\t' + obj[key] + '\n';
+            }
+        }
+    }
+    return txt;
+}
+
 
 const ItemList = (props) => {
+    console.log(RunList(props.itemList, ''));
+    console.log(this.itemListArray);
     return (
-        <div> life is beautiful </div>
+        <div> a:{this.itemListArray} </div>
     );
 }
-export default ItemList;
+
+
+const mapStateToProps = (state) => {
+    return {
+        itemList: state.itemList
+    };
+}
+
+export default connect(mapStateToProps, undefined)(ItemList);
