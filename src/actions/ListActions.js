2c6fa194131e9cea2e9ec9c69107f24027a8f057
Felipe Rodrigues <felipergr@hotmail.com>
2018-08-14 23:55:29 -0300

    Revert "removed unnecessary payload from reducers"
    This reverts commit 1848f62eac9a9357b9ac555979cd32ed6ae7b047.

--- src/actions/ListActions.js
+++ src/actions/ListActions.js
@@ -1,55 +1,59 @@
-export function ChangeItem (id, item) {
+export function ChangeItem (id, newItem) {
     return {
         type: "ITEM_CHANGE",
         payload: {
-            item: item,
+            item: newItem,
             id: id
         }
     }
 }
 
-export function AddItem (id) {
+export function AddItem (id, newItem) {
     return {
         type: "ITEM_ADD",
         payload: {
+            item: newItem,
             id: id
         }
     }
 }
 
-export function DeleteItem (id) {
+export function DeleteItem (id, newItem) {
     return {
         type: "ITEM_DELETE",
         payload: {
+            item: newItem,
             id: id
         }
     }
 }
 
-export function ToggleCollapseItem (id) {
+export function ToggleCollapseItem (id, newItem) {
     return {
         type: "ITEM_COLLAPSE_TOGGLE",
         payload: {
+            item: newItem,
             id: id
         }
     }
 }
 
-export function StrikethroughItem (id) {
+export function StrikethroughItem (id, newItem) {
     return {
         type: "ITEM_STRIKETHROUGH",
         payload: {
+            item: newItem,
             id: id
         }
     }
 }
 
-export function AddItemSameLevel (id) {
+export function AddItemSameLevel (id, newItem) {
     return {
         type: "ITEM_ADD_SAME_LEVEL",
         payload: {
+            item: newItem,
             id: id
         }
     }
 }
-
